3b97ccb830db9ecadfae845e92223ff48b081260
VladislavBounegru <vert9696@gmail.com>
2020-10-08 11:47:55 +0300

    add log to some methods in rest controllers delete unused methods from rest controllers

--- bank_api/src/main/java/bank_api/services/ClientServiceImpl.java
+++ bank_api/src/main/java/bank_api/services/ClientServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class ClientServiceImpl implements ClientService {
     private ClientDAO clientDAO;
@@ -16,8 +18,23 @@ public class ClientServiceImpl implements ClientService {
     }
 
     @Override
+    public List<Client> findAll() {
+        return null;
+    }
+
+    @Override
     @Transactional
     public Client findById(int id) {
         return clientDAO.findById(id);
+    }
+
+    @Override
+    public void save(Client client) {
+        clientDAO.save(client);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        clientDAO.deleteById(id);
     }
 }
